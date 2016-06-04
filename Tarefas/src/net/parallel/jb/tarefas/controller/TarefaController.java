4601f7f6f31b306a363962a0cc6b58735898c29e
Luiz Carvalho <luiz@i9cloud.com.br>
2016-06-04 13:02:32 -0300

    Update TarefaController.java

--- Tarefas/src/net/parallel/jb/tarefas/controller/TarefaController.java
+++ Tarefas/src/net/parallel/jb/tarefas/controller/TarefaController.java
@@ -1,7 +1,32 @@
 package net.parallel.jb.tarefas.controller;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 
 import net.parallel.jb.tarefas.model.Tarefa;
 
 public class TarefaController extends AController<Tarefa> {
+public void loadFromFile() {
+		String fileName = "Tarefas.txt";
+		try {
 
+			repositorio.clear();
+			FileReader inputFile = new FileReader(fileName);
+			BufferedReader bufferReader = new BufferedReader(inputFile);
+			String line;
+			while ((line = bufferReader.readLine()) != null) {
+				Tarefa obj = Tarefa.iniciarTarefa(line.split("\\|")[0],
+						line.split("\\|")[1]);
+				save(obj);
+			}
+			bufferReader.close();
+		} catch (Exception e) {
+			System.out.println("Erro ao ler o arquivo:" + e.getMessage());
+		}
+	}
+
+	public void saveToFile() {
 }
